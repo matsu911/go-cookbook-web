4f432e358ca34e8bf9e0898585e45b1670a9c737
Shigeaki Matsumura <matsu911@gmail.com>
2016-03-14 17:42:33 +0900

    refactoring

--- main.go
+++ main.go
@@ -22,14 +22,31 @@ func main() {
 	app.ConnectDB()
 	router := gin.Default()
 	router.Static("/assets", "public/assets")
-	router.LoadHTMLGlob("views/*")
+	router.LoadHTMLGlob("views/**/*.html")
 	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home/index.html", gin.H{
+			"title": "test",
+		})
+	})
+	router.GET("/documents/:id", func(c *gin.Context) {
 		content := template.HTML(blackfriday.MarkdownBasic(([]byte)("# test")))
-		c.HTML(http.StatusOK, "index.html", gin.H{
+		c.HTML(http.StatusOK, "documents/show.html", gin.H{
 			"title":   "test",
 			"content": content,
 		})
+	})
+	authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
+	authorized.GET("/", func(c *gin.Context) {
+	})
+	authorized.GET("/documents/:id", func(c *gin.Context) {
+		// get user, it was set by the BasicAuth middleware
+		c.JSON(http.StatusOK, gin.H{"user": "aa", "secret": "bbb"})
 	})
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
